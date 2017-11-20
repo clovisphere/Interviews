da8373dc2a20c430fcb8afa3b7a616810d4a3fac
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2017-11-20 07:29:05 +0300

    modified .txt file

--- FogCreek/SupportEngineerPosition/app.py
+++ FogCreek/SupportEngineerPosition/app.py
@@ -4,28 +4,26 @@ string = 'abcdefghijklmnopqrstuvwxyz_'
 # retrieve file content
 text = ''
 with open('text.txt', 'r') as f:
-	for line in f:
-		text += line
+    for line in f:
+        text += line
 
 a_list = {}
 
 for c in string:
-	temp = 0
-	for letter in text:
-		if letter == c:
-			temp += 1
-	a_list[c] = temp
-	
+    temp = 0
+    for letter in text:
+        if letter == c:
+            temp += 1
+    a_list[c] = temp
+
 # sorted dict values
 values = sorted(a_list.values(), reverse=True)
-word = '' # will hold the sorted string
+word = ''  # will hold the sorted string
 for i in values:
-	for key, value in a_list.items():
-		if i == value:
-			word += key	
+    for key, value in a_list.items():
+        if i == value:
+            word += key
 print('\n>Given string: {} | After sorting: {}\n'.format(string, word))
 # We then need to drop all characters that appear after (and including) the '_'
 # in the sorted string.
 print('>Answer: {}\n'.format(word.split('_')[0]))
-	
-
